270a6bccfe0f423659a7f4d59414fa1112e60cc2
Kratos-33 <seethamrajubharadwaj@gmail.com>
2024-10-20 22:26:03 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
 def call(String Imagename , String Imagetag){
     echo "This is Build stage"
-    sh "docker build -t "${Imagename}":${Imagetag} ."
+    sh "docker build -t ${Imagename}:${Imagetag} ."
 }
